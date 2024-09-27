c94efb4518d128a0adf4ffa87c5d2221999e39b3
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-09-27 21:06:08 +0700

    commit pertama saya

---
+++ percobaan.js
@@ -0,0 +1,17 @@
+// memangill modul bawaan dari node.js yaitu http 
+// untuk membuat server http
+const http = require('http')
+
+http.createServer( function(request, response) {
+    response.writeHead(200, {'Content-type': 'text/html'})
+    response.end(
+        `<h1>Selamat datang di website ajikowiyu.com</h1><hr>
+        <button>klik</button>
+        <ul>
+            <li>Ronaldo</li>
+            <li></li>
+        </ul>`
+    )
+}).listen(3000, function() {
+    console.log('Server sudah nyala, buka http://localhost:3000')
+})
