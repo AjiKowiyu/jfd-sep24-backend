0402aeed127f84915e8fdfb0c29294fd3ced6447
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-09-30 20:43:12 +0700

    file html terpisah

--- percobaan.js
+++ percobaan.js
@@ -1,35 +1,18 @@
 // memangill modul bawaan dari node.js yaitu http 
 // untuk membuat server http
 const http = require('http')
+const fs = require('fs')
 
 http.createServer( function(request, response) {
     response.writeHead(200, {'Content-type': 'text/html'})
 
     // halaman utama
     if (request.url == '/') {
-        response.end(
-            `<h1>Selamat datang di website ajikowiyu.com</h1><hr>
-            <a href="/profil">Lihat Profil</a>`
-        )
+        fs.createReadStream('./view/halaman-utama.html').pipe(response)
     }
     // halaman profil
     else if (request.url == '/profil') {
-        let tahun_lahir = 1990
-        let tahun_ini = 2024
-        let umur = tahun_ini - tahun_lahir
-
-        response.end(
-            `<ul>
-                <li>Nama lengkap: Aji Kowiyu</li>
-                <li>Nama panggilan: Aji/Kowi</li>
-                <li>Alamat: Pluit, Jakarta Utara</li>
-                <li>Pekerjaan: Senior Programmer @ Agung Podomoro Group</li>
-                <li>tanggal lahir: 17 Agustus ${tahun_lahir}</li>
-                <li>umur: ${umur} tahun</li>
-            </ul>
-            <br>
-            <a href="/">Balik ke beranda</a>`
-        )
+        fs.createReadStream('./view/halaman-profil.html').pipe(response)
     }
     // halaman hubungi-saya
     else if (request.url == '/hubungi-saya') {
