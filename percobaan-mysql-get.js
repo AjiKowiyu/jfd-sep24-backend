59082fb5657493c8613f189c82b84a63f4ea4594
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-10-01 19:55:05 +0700

    kombinasi request db & server web

--- percobaan-mysql-get.js
+++ percobaan-mysql-get.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql2')
+const http  = require('http')
+const fs    = require('fs')
+
 
 // sambungkan ke mysql
 const db = mysql.createConnection({
@@ -20,10 +23,45 @@ db.connect( (error)=>{
 
 
 
-db.query('SELECT * FROM karyawan', function(errorSql, hasil) {
-    if (errorSql) {
-        console.log(errorSql)
-    } else {
-        console.log(hasil)
+
+function getAll_karyawan() {
+    return new Promise( (resolve, reject) => {
+        db.query('SELECT * FROM karyawan', function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)
+            } else {
+                resolve(hasil)
+            }
+        })
+    })
+}
+
+
+
+
+
+http.createServer( async function(request, response) {
+    response.writeHead(200, {'Content-type': 'text/html'})
+
+    if (request.url == '/') {
+        fs.createReadStream('./view/halaman-utama.html').pipe(response)
     }
+    else if (request.url == '/karyawan') {
+
+        // tarik data dari db
+        let data = await getAll_karyawan()
+
+        // kirim hasilnya ke front-end
+        response.end(
+            `<h1>Data Karyawan PT Data Informasi Teknologi</h1>
+            <hr>
+            Nama Lengkap: ${data[0].nama} <br>
+            Nomor Induk Karyawan: ${data[0].nik} <br>
+            <pre>
+                ${JSON.stringify(data, null, 4)}
+            </pre>`
+        )
+    }
+}).listen(3000, function() {
+    console.log('Server aktif, buka http://localhost:3000')
 })
