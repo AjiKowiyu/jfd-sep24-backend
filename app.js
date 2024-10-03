ec0a5398397b37fbfbf755619b0398ea9960141f
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-10-03 20:23:14 +0700

    karyawan detail

--- app.js
+++ app.js
@@ -37,6 +37,31 @@ function getAll_karyawan() {
 }
 
 
+
+function getOne_karyawan() {
+    return new Promise( (resolve, reject) => {
+        let sqlSyntax =
+        `SELECT 
+            kry.*, jbt.nama as jabatan_nama, 
+            jbt.singkatan as jabatan_singkatan, 
+            agm.nama as agama_nama 
+        FROM karyawan as kry 
+        LEFT JOIN jabatan as jbt ON jbt.id = kry.jabatan 
+        LEFT JOIN agama as agm ON agm.id = kry.agama
+        WHERE kry.id = 13`
+
+        db.query(sqlSyntax, function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)
+            } else {
+                resolve(hasil)
+            }
+        })
+    })
+}
+
+
+
 app.set('view engine', 'ejs')   //setting penggunaan template engine untuk express
 app.set('views', './view-ejs')  //setting penggunaan folder untuk menyimpan file .ejs
 
@@ -67,6 +92,15 @@ app.get('/karyawan', async function(req, res) {
         karyawan: await getAll_karyawan()
     }
     res.render('page-karyawan', data)
+})
+
+
+app.get('/karyawan/detail', async function(req,res) {
+    // ambil data karyawan 1 aja
+    let data = {
+        satukaryawan: await getOne_karyawan()
+    }
+    res.render('page-karyawan-detail', data)
 })
 
 
