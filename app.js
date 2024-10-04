2a25190d93de9fb79b12d26aa827e87145a88c90
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-10-04 21:50:03 +0700

    cara modern penulisan script insert

--- app.js
+++ app.js
@@ -195,19 +195,31 @@ app.post('/karyawan/proses-insert-data', formValidasiInsert, async function(req,
 function insert_karyawan( req ) {
     return new Promise( (resolve, reject) => {
         let sqlSyntax =
-        `INSERT INTO karyawan
-        (nama, nik, tanggal_lahir, alamat, jabatan, agama)
-        VALUES
-        (?, ?, ?, ?, ?, ?)`
+        `INSERT INTO karyawan SET ?`
 
-        let sqlData = [
-            req.body.form_nama,
-            req.body.form_nik,
-            req.body.form_tanggal_lahir,
-            req.body.form_alamat,
-            req.body.form_jabatan,
-            req.body.form_agama,
-        ]
+        let sqlData = {
+            nama            : req.body.form_nama,
+            nik             : req.body.form_nik,
+            tanggal_lahir   : req.body.form_tanggal_lahir,
+            alamat          : req.body.form_alamat,
+            jabatan         : req.body.form_jabatan,
+            agama           : req.body.form_agama,
+        }
+
+        // let sqlSyntax =
+        // `INSERT INTO karyawan
+        // (nama, nik, tanggal_lahir, alamat, jabatan, agama)
+        // VALUES
+        // (?, ?, ?, ?, ?, ?)`
+
+        // let sqlData = [
+        //     req.body.form_nama,
+        //     req.body.form_nik,
+        //     req.body.form_tanggal_lahir,
+        //     req.body.form_alamat,
+        //     req.body.form_jabatan,
+        //     req.body.form_agama,
+        // ]
 
         db.query(sqlSyntax, sqlData, function(errorSql, hasil) {
             if (errorSql) {
