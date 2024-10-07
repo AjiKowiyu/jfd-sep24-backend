1394aeb78ef05722f4db6e298c1085c47d9ae1de
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-10-07 22:01:28 +0700

    controller karyawana

--- app.js
+++ app.js
@@ -32,123 +32,20 @@ app.set('view engine', 'ejs')
 app.set('views', './view-ejs')
 
 
-// function render('nama-file')
-// nama file nya wajib berekstensi .ejs
-// otomatis mengambil file .ejs yg ada di folder view-ejs
-
-
 app.get('/', cont_default.halaman_beranda)
 app.get('/pendidikan', cont_pendidikan.halaman_pendidikan)
-
-
-app.get('/karyawan', async function(req, res) {
-    // proses penarikan data
-    let data = {
-        karyawan: await model_karyawan.getAll_karyawan(),
-        notifikasi: req.query.notif,
-    }
-    res.render('page-karyawan', data)
-})
-
-
-app.get('/karyawan/detail/:id_karyawan', async function(req,res) {
-    // ambil data karyawan 1 aja
-    let data = {
-        satukaryawan: await model_karyawan.getOne_karyawan( req.params.id_karyawan )
-    }
-    res.render('page-karyawan-detail', data)
-})
-
-
-
-
-
-app.get('/karyawan/tambah', async function(req,res) {
-    let data = {
-        jabatan: await model_jabatan.getAll_jabatan(),
-        agama: await model_agama.getAll_agama(),
-    }
-    res.render('page-karyawan-form-tambah', data)
-})
-
-
-
+app.get('/karyawan', cont_karyawan.halaman_list_semua_karyawan)
+app.get('/karyawan/detail/:id_karyawan', cont_karyawan.halaman_karyawan_detail)
+app.get('/karyawan/tambah', cont_karyawan.halaman_karyawan_form_tambah)
 
 let formValidasiInsert = [
     body('form_nik').notEmpty().isNumeric(),
     body('form_nama').notEmpty().isString(),
 ]
-
-app.post('/karyawan/proses-insert-data', formValidasiInsert, async function(req,res) {
-    const errors = validationResult(req)
-    if (errors.isEmpty()) {
-        try {
-            let insert = await model_karyawan.insert_karyawan( req )
-            if (insert.affectedRows > 0) {
-                res.redirect('/karyawan?notif=Berhasil input karyawan baru')
-            }
-        } catch (error) {
-            throw error
-        }
-    }
-    else {
-        let errorData = {
-            pesanError: errors.array()
-        }
-        errorData.pesanError[0].fields
-        res.render('page-karyawan-form-tambah', errorData)
-    }
-})
-
-
-
-
-
-
-
-
-
-
-app.get('/karyawan/hapus/:id_karyawan', async function(req,res) {
-    try {
-        let hapus = await model_karyawan.hapusKaryawan( req.params.id_karyawan )
-        if (hapus.affectedRows > 0) {
-            res.redirect('/karyawan')
-        }
-    } catch (error) {
-        throw error
-    }
-})
-
-
-
-app.get('/karyawan/edit/:id_karyawan', async function(req,res) {
-    let data = {
-        satukaryawan: await model_karyawan.getOne_karyawan( req.params.id_karyawan ),
-        jabatan: await model_jabatan.getAll_jabatan(),
-        agama: await model_agama.getAll_agama(),
-        moment: moment,
-    }
-    res.render('page-karyawan-form-edit', data)
-})
-
-
-
-app.post('/karyawan/proses-update-data/:id_karyawan', async function(req,res) {
-    try {
-        let update = await model_karyawan.update_karyawan( req )
-        if (update.affectedRows > 0) {
-            res.redirect('/karyawan?notif=Berhasil perbarui data karyawan')
-        }
-    } catch (error) {
-        
-    }
-})
-
-
-
-
-
+app.post('/karyawan/proses-insert-data', formValidasiInsert, cont_karyawan.proses_insert_karyawan)
+app.get('/karyawan/hapus/:id_karyawan', cont_karyawan.proses_hapus)
+app.get('/karyawan/edit/:id_karyawan', cont_karyawan.halaman_karyawan_form_edit)
+app.post('/karyawan/proses-update-data/:id_karyawan', cont_karyawan.proses_update)
 
 
 app.listen(3000, ()=>{
